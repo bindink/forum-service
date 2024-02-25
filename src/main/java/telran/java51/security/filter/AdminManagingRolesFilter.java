8a1d9b4c3d95b54e7abd06117402b7a7e73604da
Dina <bindink@gmail.com>
2024-02-25 16:36:08 +0200

    Filters for add/update/delete post ,add comment to post

---
+++ src/main/java/telran/java51/security/filter/AdminManagingRolesFilter.java
@@ -0,0 +1,41 @@
+package telran.java51.security.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import telran.java51.accounting.dao.AccountRepository;
+import telran.java51.accounting.model.Role;
+import telran.java51.accounting.model.User;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Order(30)
+public class AdminManagingRolesFilter implements Filter {
+    final AccountRepository accountRepository;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (checkEndPoint(request.getMethod(), request.getServletPath())) {
+            User user = accountRepository.findById(request.getUserPrincipal().getName()).get();
+            if (!user.getRoles().contains(Role.ADMINISTRATOR)) {
+                response.sendError(403, "Permission denied");
+                return;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+    private boolean checkEndPoint(String method, String path) {
+        return path.matches("/account/user/\\w+/role/\\w+");
+    }
+
+}
