2503360cd0bc40af9bb3ec44f0b0739f357d8007
Dina <bindink@gmail.com>
2024-03-03 18:33:11 +0200

    Add Session Id to cookies

--- src/main/java/telran/java51/security/AuthorizationConfiguration.java
+++ src/main/java/telran/java51/security/AuthorizationConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 import telran.java51.accounting.model.Role;
@@ -19,6 +20,7 @@ public class AuthorizationConfiguration {
     public SecurityFilterChain web(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS));
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/account/register", "/forum/posts/**")
                     .permitAll()
